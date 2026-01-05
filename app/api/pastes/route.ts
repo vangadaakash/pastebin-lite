db5a3119d7d4aeb22a824928abcc3f34a2cf793f
Akash <vangadaakash529@gmail.com>
2026-01-05 21:13:59 +0530

    fix base url for vercel

--- app/api/pastes/route.ts
+++ app/api/pastes/route.ts
@@ -57,7 +57,10 @@ export async function POST(request: Request) {
     );
 
     // 7️⃣ Build URL
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+    const baseUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
+
 
     return NextResponse.json(
       {
